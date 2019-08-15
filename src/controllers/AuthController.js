390011aa224ef51a01a7b3275a50a3ca65a03972
Nikolas Lencioni <nikolaslencioni@MacBook-Air-de-Nikolas.local>
2019-08-15 18:48:47 -0300

    LIXO_DE_LINGUAGEM

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,38 +1,77 @@
-const db = require('../config/database')
 const { generateToken } = require('../helpers/AuthHelper')
+const { atou, md5 } = require('../helpers/AuthHelper')
+const { findUserByCpf, findUserByCnpj } = require('../services/UserService')
+
+function _tryPass(partial, response, co, i) {
+   if (i == ((partial.length) - 1)) {
+      response.push([md5(co+partial[i][0])])
+      response.push([md5(co+partial[i][1])])
+      response.push([md5(co+partial[i][2])])
+   } else {
+      _tryPass(partial, response, co+partial[i][0], i+1)
+      _tryPass(partial, response, co+partial[i][1], i+1)
+      _tryPass(partial, response, co+partial[i][2], i+1)
+   }
+   return response
+}
 
 module.exports = {
+
    async login(req, res) {
       try {
-         const data = req.body
+         const input = req.body
 
-         if (data.radio === 1) {
-            var user = await db('pessoa').where('cpf_pes', data.username).first()
-         } else if (data.radio === 2) {
-            var user = await db('pessoa').where('cnpj_pes', data.username).first()
+         if (input.radio === 1) {
+            var user = findUserByCpf(input.username)
+         } else if (input.radio === 2) {
+            var user = findUserByCnpj(input.username)
          }
 
-         if (!user)
-            return res.status(400).json({ error: "User not found!" })
+         user.then(result => {
+            if (!result)
+               return res.json({error: 'Usuário não encontrado!'})
+         })
+
+         var arrayPass = input.password.split('fQ==')
+         var partial = []
+         
+         arrayPass.forEach(element => {
+            
+            var button = []
+            var num = atou(element).substr(1).split(',')
+            
+            num.forEach(value => {
+               button.push([value])
+            })
+            partial.push(button)
+         })
+         
+         partial.pop()
+         
+         var response = _tryPass(partial, [], '', 0)
+
+         const data = {
+            username: input.username,
+            password: response
+         }
 
          // Aqui metodos de autenticaçao e verificaçao de password
 
-         user.token = generateToken(user)
+         // user.token = generateToken(user)
          user.senha_pes = undefined
 
-         return res.json({
-            user
-         })
+         return res.json({ user })
 
       } catch (error) {
-         return res.status(400).json({ error: "User authentication failed! Try again later. Error: " + error })
+         console.log(error)
+         return res.json({ error: "Autenticação do usuário falhou. Tente novamente mais tarde." })
       }
    },
 
    async recoverPassword(req, res) {
       try {
          const data = req.body
-         
+
          if (data.radio === 1) {
             var user = await db('pessoa').where('cpf_pes', data.username).first()
          } else if (data.radio === 2) {
@@ -49,7 +88,7 @@ module.exports = {
          })
 
       } catch (error) {
-         return res.status(400).json({ error: "Recover password failed! Try again later. Error: " + error })         
+         return res.status(400).json({ error: "Recover password failed! Try again later. Error: " + error })
       }
    }
 }
